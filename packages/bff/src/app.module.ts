6fb4784b96eb5082a1eff0669834bc1b695ea804
tsucchinoko <ccya2211@icloud.com>
2026-03-14 16:30:14 +0900

    fix: .envの位置を移動

--- packages/bff/src/app.module.ts
+++ packages/bff/src/app.module.ts
@@ -1,15 +1,15 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { WeatherModule } from './weather/weather.module';
-import { ExternalApiModule } from './external-api/external-api.module';
-import { CacheConfigModule } from './cache/cache.module';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { WeatherModule } from "./weather/weather.module";
+import { ExternalApiModule } from "./external-api/external-api.module";
+import { CacheConfigModule } from "./cache/cache.module";
 
 @Module({
   imports: [
     // 環境変数の読み込み (.envファイル対応)
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '../../.env',
+      envFilePath: ".env",
     }),
     CacheConfigModule,
     ExternalApiModule,
