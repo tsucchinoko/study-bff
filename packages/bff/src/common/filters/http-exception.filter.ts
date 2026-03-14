38595f9fa042096850fb1b6a80c1b1e7c69b4f51
tsucchinoko <ccya2211@icloud.com>
2026-03-14 17:51:08 +0900

    refactor: 重複ロジックの集約

--- packages/bff/src/common/filters/http-exception.filter.ts
+++ packages/bff/src/common/filters/http-exception.filter.ts
@@ -17,18 +17,11 @@ import { Response } from 'express';
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
+    const response = host.switchToHttp().getResponse<Response>();
 
-    const status =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
-
-    const message =
-      exception instanceof HttpException
-        ? exception.message
-        : 'Internal server error';
+    const isHttpException = exception instanceof HttpException;
+    const status = isHttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+    const message = isHttpException ? exception.message : 'Internal server error';
 
     response.status(status).json({
       statusCode: status,
